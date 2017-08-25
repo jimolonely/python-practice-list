02ba1231ce4cb40a9a99699932777b4d6d9c18be
root <15982406684@163.com>
2017-08-25 11:40:32 +0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+str = raw_input('输入字符串：')
+
+vowels = ['a','i','o','u','e']
+
+count = {}
+
+for s in str:
+    if s in vowels:
+        count[s] = count.get(s,0) + 1
+
+for k,v in count.items():
+    print('%s出现了%d次'%(k,v))
